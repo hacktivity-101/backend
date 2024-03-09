7f2d1a693e7696db840e0a5acc0c9e45e7a917ed
hacktivity-101 <maddyakash2136@gmail.com>
2024-03-09 20:13:12 +0530

    express

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-//import
+// import
 const express = require('express');
 
 // initialize
 const app = express();
 
-const postRouter = require('./routers/postRouter');
-
-//middleware
-app.use('/post', postRouter);
-
 const port = 5000;
 
 app.get('/', (req, res) => {
     res.send('response from express');
-})
+});
+
 app.get('/add', (req, res) => {
     res.send('response from add route');
 })
 
-
-app.listen(port, () => { console.log('express server started') });
+app.listen(port, () => { console.log('express server started now') });
